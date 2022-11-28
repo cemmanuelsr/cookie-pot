1f8967f827bc394850a6a6e522c0e90fc4153edf
cemmanuelsr <caioemmanuelsr@gmail.com>
2022-11-28 20:05:10 -0300

    getting third party connections

--- cookies.js
+++ cookies.js
@@ -1,3 +1,7 @@
+function asyncDelay(time) {
+  return new Promise(res => setTimeout(res, time));
+}
+
 function showCookiesForTab(tabs) {
   let tab = tabs.pop();
 
@@ -34,38 +38,80 @@ function showCookiesForTab(tabs) {
         document.getElementById('cookies-header').style.display = 'block';
       }
     }
+
+    return cookies.length ? secureNumberOfCookies / cookies.length : 0.5;
   });
 }
 
 function showStorageForTab(storage) {
   if (storage.length > 0) {
     let activeTabUrl = document.getElementById('header-storage-title');
-    let text = document.createTextNode(`Local Storage has ${storage.length} items`);
-    activeTabUrl.appendChild(text);
 
     let localStorageList = document.getElementById('local-storage-list');
+    let totalSize = 0;
     for (let i in storage) {
       let item = storage[i];
       let li = document.createElement("li");
       let content = document.createTextNode(item);
       li.appendChild(content);
       localStorageList.appendChild(li);
+      totalSize += (item.length + i.length) * 2;
     }
 
     if (localStorageList.children.length > 0) {
       document.getElementById('local-storage-container').style.display = 'block';
     }
+
+    let text = document.createTextNode(`Local Storage has ${storage.length} items (${(totalSize / 1024.0).toFixed(2)} KB)`);
+    activeTabUrl.appendChild(text);
+
+    return 1.0 - (totalSize / 1024.0) / 5120.0;
   }
+
+  return 0.5;
+}
+
+function logRequestLength() {
+  let requests = browser.extension.getBackgroundPage().getRequests();
+
+  if (requests.length > 0) {
+    let activeTabUrl = document.getElementById('network-title');
+
+    let networkList = document.getElementById('network-list');
+    let thirdPartyRequests = 0;
+    for (let i in requests) {
+      let request = requests[i];
+      let li = document.createElement("li");
+      let content = document.createTextNode(`${request.method} - ${request.url}`);
+      li.appendChild(content);
+      networkList.appendChild(li);
+      thirdPartyRequests += (request.thirdParty === true);
+    }
+
+    if (networkList.children.length > 0) {
+      document.getElementById('network-container').style.display = 'block';
+    }
+
+    let text = document.createTextNode(`${thirdPartyRequests} third party connections`);
+    activeTabUrl.appendChild(text);
+
+    return 1.0 - thirdPartyRequests / requests.length;
+  }
+
+  return 0.5;
 }
 
 function getActiveTab() {
   return browser.tabs.query({ currentWindow: true, active: true });
 }
-getActiveTab().then(showCookiesForTab);
 
-browser.tabs.executeScript({
-  file: "js/getLocalStorage.js"
-}).then((values) => {
-  showStorageForTab(values[0]);
+getActiveTab().then(showCookiesForTab).then(() => {
+  browser.tabs.executeScript({
+    file: "js/getLocalStorage.js"
+  }).then((values) => {
+    showStorageForTab(values[0]);
+  }).then(() => {
+    logRequestLength().then((data) => { console.log(data.length) })
+  });
 });
 
--- js/utils.js
+++ js/utils.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
   $("#cookie-list").toggle();
   $("#local-storage-list").toggle();
+  $("#network-list").toogle();
   $("#cookies-title").click(function() {
     $("#cookie-list").toggle();
   });
   $("#local-storage-title").click(function() {
     $("#local-storage-list").toggle();
+  });
+  $("#network-title").click(function() {
+    $("#network-list").toggle();
   });
 });
